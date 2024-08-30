305009bc494c7bb2318fee27303309e57cb0cf73
Nithin AG <nithinag10@gmail.com>
2024-08-30 18:35:57 +0530

    changed the config file

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,17 @@ import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: azure()
+	adapter: adapter({
+		// default options are shown. On some platforms
+		// these options are set automatically — see below
+		pages: 'build',
+		assets: 'build',
+		fallback: undefined,
+		precompress: false,
+		strict: true
+	}),
+	paths: {
+		relative: false
 	}
 };
 
