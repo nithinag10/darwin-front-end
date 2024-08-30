bd425c9ba83b7e7971e94d65b539cd208813ba80
Nithin AG <nithinag10@gmail.com>
2024-08-30 22:31:49 +0530

    removed unwanted workflow and changed workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	adapter: adapter({
-		// default options are shown. On some platforms
-		// these options are set automatically — see below
-		pages: 'build',
-		assets: 'build',
-		fallback: null,
-		precompress: false,
-		strict: true
-	}),
-	prerender: {
-		// This can be false if you're using a fallback (i.e. SPA mode)
-		default: true
-	}
-
+	adapter: azure({
+		// Empty options object for default settings
+	})
 };
 
 export default config;
